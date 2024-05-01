34d43c9ee1114b1acae42281c24c3f23d9878e4a
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-05-02 02:18:36 +0600

    more-map

---
+++ more-map1.js
@@ -0,0 +1,15 @@
+const numbers = [12, 10, 8, 15, 7];
+
+const doubled = numbers.map((num) => num * 2);
+// console.log(doubled);
+fiveBonus = numbers.map((num) => num + 5);
+// console.log(fiveBonus);
+const halves = numbers.map((num) => num / 2);
+// console.log(halves);
+
+const friends = ["tom", "john", "michel", "oliver"];
+
+const lengths = friends.map((frnd) => frnd.length);
+// console.log(lengths)
+const firstLetter = friends.map((friends) => friends[0]);
+console.log(firstLetter);
