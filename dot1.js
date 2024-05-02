4924d8b5b414664d7c5c352df47b9f5fda51eae6
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-05-03 02:06:24 +0600

    dot notation

---
+++ dot1.js
@@ -0,0 +1,10 @@
+const person = {
+	name: "kodom ali",
+	job: "badam khai",
+	3: "third",
+};
+const prop = "job";
+console.log(person.job);
+console.log(person["job"]);
+console.log(person[prop]);
+console.log(person['3']);
